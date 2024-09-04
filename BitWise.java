5e74a77cd7a01be9e1d7386c9840a4daf1a0fd41
Hajarath21 <143639084+Hajarath21@users.noreply.github.com>
2024-09-04 16:35:16 +0530

    Add files via upload

---
+++ BitWise.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class BitWise {
+    public static void main(String[] args) {
+        int n = 5;
+        int pos = 3;
+        int bitMask = 1 << pos;
+        if ((bitMask & n) == 0) {
+            System.out.println("kjhgfds");
+        } else {
+            System.out.println("oiuytre");
+        }
+    }
+}
